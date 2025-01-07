0c73d0c9a7308b27da5208d42216a5b835f937f6
Vadim Sofin <sofin.vadim.shared@gmail.com>
2025-01-07 20:31:16 +0400

    Task 24: add provides directive to implementations in module-info files

--- Java_8_to_17/homework/jmp-cloud-service-impl/src/main/java/module-info.java
+++ Java_8_to_17/homework/jmp-cloud-service-impl/src/main/java/module-info.java
@@ -2,4 +2,5 @@ module jmp.cloud.service.impl {
     requires transitive jmp.service.api;
     requires jmp.dto;
     exports com.jmp.cloud.service.impl;
+    provides com.jmp.service.api.Service with com.jmp.cloud.service.impl.CloudService;
 }
