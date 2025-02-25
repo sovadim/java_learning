6f3b5cc0b08b4b6046ed17ebbfd9170cc61efc3b
Vadim Sofin <sofin.vadim.shared@gmail.com>
2025-02-25 19:31:29 +0400

    Add empty get/set methods to Client and TDD test

--- Testing_in_Java/messenger/src/main/java/com/epam/ld/module2/testing/Client.java
+++ Testing_in_Java/messenger/src/main/java/com/epam/ld/module2/testing/Client.java
@@ -23,4 +23,24 @@ public class Client {
     public void setAddresses(String addresses) {
         this.addresses = addresses;
     }
+
+    /**
+     * Gets value for template by key, if exists.
+     *
+     * @param key template key
+     * @return value
+     */
+    public String getValue(final String key) {
+        return null;
+    }
+
+    /**
+     * Add template value.
+     *
+     * @param key template key
+     * @param value template value for key
+     */
+    public void addTemplateValue(final String key,
+                                 final String value) {
+    }
 }
