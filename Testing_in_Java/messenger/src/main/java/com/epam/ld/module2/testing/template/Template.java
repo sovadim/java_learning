49453ff92af9daab2b7d686b31e8e8fe3a54fd81
Vadim Sofin <sofin.vadim.shared@gmail.com>
2025-02-25 16:00:50 +0400

    Simple case: template returns same text

--- Testing_in_Java/messenger/src/main/java/com/epam/ld/module2/testing/template/Template.java
+++ Testing_in_Java/messenger/src/main/java/com/epam/ld/module2/testing/template/Template.java
@@ -4,4 +4,13 @@ package com.epam.ld.module2.testing.template;
  * The type Template.
  */
 public class Template {
+    private final String template;
+
+    public Template(String template) {
+        this.template = template;
+    }
+
+    public String getTemplate() {
+        return template;
+    }
 }
--- Testing_in_Java/messenger/src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
+++ Testing_in_Java/messenger/src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
@@ -1,4 +1,29 @@
 package com.epam.ld.module2.testing.template;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class TemplateEngineTest {
+    TemplateEngine templateEngine;
+
+    @BeforeEach
+    public void setUp() {
+         templateEngine = new TemplateEngine();
+    }
+
+    @Test
+    @DisplayName("Empty template results in empty text")
+    public void emptyTemplateResultsInEmptyTextTest() {
+        assertTrue(templateEngine.generateMessage(new Template(""), null).isEmpty());
+    }
+
+    @Test
+    @DisplayName("Template with no placeholders returns same text")
+    public void templateWithNoPlaceholdersReturnsSameTextTest() {
+        assertEquals("Some text", templateEngine.generateMessage(new Template("Some text"), null));
+    }
 }
