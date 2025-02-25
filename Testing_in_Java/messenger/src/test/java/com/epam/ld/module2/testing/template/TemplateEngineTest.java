94501777b910a0bd702fb71b48562d20986b3305
Vadim Sofin <sofin.vadim.shared@gmail.com>
2025-02-25 19:41:46 +0400

    Add util test methods and refactor tests for template engline

--- Testing_in_Java/messenger/src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
+++ Testing_in_Java/messenger/src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
@@ -1,29 +1,42 @@
 package com.epam.ld.module2.testing.template;
 
+import com.epam.ld.module2.testing.Client;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TemplateEngineTest {
     TemplateEngine templateEngine;
+    Client client;
 
     @BeforeEach
     public void setUp() {
          templateEngine = new TemplateEngine();
+         client = new Client();
+    }
+
+    private String generateMessage(String template) {
+        return templateEngine.generateMessage(new Template(template), client);
+    }
+
+    private void addValue(String key, String value) {
+        client.addTemplateValue(key, value);
     }
 
     @Test
     @DisplayName("Empty template results in empty text")
     public void emptyTemplateResultsInEmptyTextTest() {
-        assertTrue(templateEngine.generateMessage(new Template(""), null).isEmpty());
+        assertTrue(generateMessage("").isEmpty());
     }
 
     @Test
     @DisplayName("Template with no placeholders returns same text")
     public void templateWithNoPlaceholdersReturnsSameTextTest() {
-        assertEquals("Some text", templateEngine.generateMessage(new Template("Some text"), null));
+        assertEquals(
+                "Some text",
+                generateMessage("Some text")
+        );
     }
 }
