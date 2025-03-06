a6efcd8d34fd953accc53fc73cdcc4cd1ae66ed8
Vadim Sofin <sofin.vadim.shared@gmail.com>
2025-03-06 16:02:06 +0400

    Implement sending message in MailServer into desired channel

--- Testing_in_Java/messenger/src/main/java/com/epam/ld/module2/testing/MailServer.java
+++ Testing_in_Java/messenger/src/main/java/com/epam/ld/module2/testing/MailServer.java
@@ -1,9 +1,21 @@
 package com.epam.ld.module2.testing;
 
+import java.io.PrintWriter;
+
 /**
  * Mail server class.
  */
 public class MailServer {
+    private final PrintWriter writer;
+
+    /**
+     * Initializes a new MailServer with desired output channel.
+     *
+     * @param writer the output stream
+     */
+    public MailServer(final PrintWriter writer) {
+        this.writer = writer;
+    }
 
     /**
      * Send notification.
@@ -13,5 +25,6 @@ public class MailServer {
      */
     public void send(final String addresses,
                      final String messageContent) {
+        writer.write(messageContent);
     }
 }
