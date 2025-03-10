5396de8c04221d0b2e4e07a734386f8b5371f4a8
Vadim Sofin <sofin.vadim.shared@gmail.com>
2025-03-10 15:15:13 +0400

    Add TDD tests on App in file mode

--- Testing_in_Java/messenger/src/test/java/com/epam/ld/module2/testing/AppConsoleModeTest.java
+++ Testing_in_Java/messenger/src/test/java/com/epam/ld/module2/testing/AppConsoleModeTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Tag("AppTests")
-public class AppTest {
+public class AppConsoleModeTest {
     static ByteArrayOutputStream output;
 
     @BeforeEach
