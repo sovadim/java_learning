14b78564679bb748ab04359b0aace10066644551
Vadim Sofin <sofin.vadim.shared@gmail.com>
2025-03-27 15:54:39 +0400

    Add check that file exists

---
+++ Files/homework/DiskAnalyzer/src/main/java/com/epam/files/FileFinder.java
@@ -0,0 +1,9 @@
+package com.epam.files;
+
+import java.nio.file.Path;
+
+public class FileFinder {
+    public void findFileWithMaxS(Path path) {
+        System.out.println("TODO: Task 1");
+    }
+}
