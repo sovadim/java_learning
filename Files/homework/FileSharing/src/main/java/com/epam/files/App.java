dc7473d1acb023509fb65bdf76700e0df3b30b77
Vadim Sofin <sofin.vadim.shared@gmail.com>
2025-04-12 21:52:36 +0400

    Initialize FileSharing project

---
+++ Files/homework/FileSharing/src/main/java/com/epam/files/App.java
@@ -0,0 +1,11 @@
+package com.epam.files;
+
+public class App {
+    private final static String helpString =
+            """
+            """;
+
+    public static void main(String[] args) {
+
+    }
+}
