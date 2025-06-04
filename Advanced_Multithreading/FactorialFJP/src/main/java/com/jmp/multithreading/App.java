cf7a38d3a03832a93ef54c6c8e8139a3b7751af0
Vadim Sofin <Vadim_Sofin@epam.com>
2025-06-04 18:19:13 +0400

    Init task 1

---
+++ Advanced_Multithreading/FactorialFJP/src/main/java/com/jmp/multithreading/App.java
@@ -0,0 +1,10 @@
+package com.jmp.multithreading;
+
+public class App {
+    private static final String helpString =
+            """
+            """;
+
+    public static void main(String[] args) {
+    }
+}
